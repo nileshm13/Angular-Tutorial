8e205200bcb71297352fd9b29836a510e35fd471
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-01-24 14:09:41 +0530

    ViewChild on HTML Element

--- angular-ekart/src/app/container/search/search.component.ts
+++ angular-ekart/src/app/container/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -10,10 +10,28 @@ export class SearchComponent {
   
   @Output()
   searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
+  @ViewChild('searchInputRef') searchInputElement: ElementRef
+  // updateSearchText(searchTextRef: HTMLInputElement) {
+  //Template Referencing the input textbox via searchTextRef; for template referencing, just use #name_of_your_choice besides an HTML element 
+  //   this.searchText = searchTextRef.value;  
+  //   this.searchTextChanged.emit(this.searchText);
+  //   console.log(this.searchText);
+  // }
 
-  updateSearchText(searchTextRef: any) {
-    this.searchText = searchTextRef.value;  //Template Referencing the input textbox via searchTextRef; for template referencing, just use #name_of_your_choice besides an HTML element 
+  // Template Referencing used above will only be available on button click, but what if we need it as soon as page laod, for it we use @ViewChild
+  // @ViewChild allows us to access DOM, Directive or Component
+  //   ViewChild(selector: string | Function | ProviderToken<unknown>, opts?: {
+  //     read?: any;
+  //     static?: boolean;
+  // }): any
+  // selector - The directive type or the name used for querying.
+  // read - Used to read a different token from the queried elements.
+  // static - True to resolve query results before change detection runs, false to resolve after change detection. Defaults to false.
+
+  updateSearchText() {
+    this.searchText = this.searchInputElement.nativeElement.value;  //Template Referencing the input textbox via searchTextRef; for template referencing, just use #name_of_your_choice besides an HTML element 
     this.searchTextChanged.emit(this.searchText);
     console.log(this.searchText);
   }
+
 }
