26abcc8b34ee13a211e7cc79dfd51d938adc629d
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-01-24 15:55:10 +0530

    ViewChild for Component

---
+++ angular-ekart/src/app/container/product-details/product-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { Product } from 'src/app/Models/product';
+import { ProductListComponent } from '../product-list/product-list.component';
+
+@Component({
+  selector: 'product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent {
+  @Input()
+  productListComp: ProductListComponent = undefined;
+  product: Product;
+
+  ngOnInit() {
+    this.product = this.productListComp.selectedProduct;    
+  }
+
+}
--- angular-ekart/src/app/container/product-list/product/product.component.ts
+++ angular-ekart/src/app/container/product-list/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Input } from '@angular/core';
 import { Component } from '@angular/core';
+import { Product } from './../../../Models/product'
 
 @Component({
   selector: 'app-product',
@@ -8,20 +9,5 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
   @Input()
-  product: {
-    id: number
-    name: string
-    description: string
-    brand: string
-    gender: string
-    category: string
-    size: number[],
-    color: string[],
-    price: number,
-    discountPrice?:number,
-    is_in_inventory: boolean,
-    items_left: number,
-    imageURL: string
-    slug: string
-  };
+  product: Product
 }
