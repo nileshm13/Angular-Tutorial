69e58e091174bfd9c2c46671ab940192b41aa8dc
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-01-24 16:56:07 +0530

    ng-template in Angular

---
+++ angular-ekart/src/app/container/ng-template-example/ng-template-example.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-template-example',
+  templateUrl: './ng-template-example.component.html',
+  styleUrls: ['./ng-template-example.component.css']
+})
+export class NgTemplateExampleComponent {
+
+}
