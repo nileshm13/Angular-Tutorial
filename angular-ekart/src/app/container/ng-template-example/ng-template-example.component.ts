ffa06f9eee59e4819c8dd27fed753d9ef5bd6b44
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-01-24 17:52:59 +0530

    ng-container in Angular

--- angular-ekart/src/app/container/ng-template-example/ng-template-example.component.ts
+++ angular-ekart/src/app/container/ng-template-example/ng-template-example.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ng-template-example',
+  selector: 'app-ng-template-example',
   templateUrl: './ng-template-example.component.html',
   styleUrls: ['./ng-template-example.component.css']
 })
