d63728f5e240d81c2f39ba8c58bf775788823949
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-02 18:05:46 +0530

    Renderer2 in Angular

--- angular-ekart/src/app/container/CustomDirectives/SetBackground.directive.ts
+++ angular-ekart/src/app/container/CustomDirectives/SetBackground.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 //this directive is assigned to span element in product details, can be assigned to any other div or other element as well
 @Directive({
@@ -7,9 +7,11 @@ import { Directive, ElementRef, OnInit } from "@angular/core";
 
 export class SetBackground implements OnInit {
     // private element: ElementRef;
-    //constructor(element: ElementRef) {
-        constructor(private element: ElementRef) {  //this creates a private variable name element of type ElementRef and assigns whatever value gets injected(template reference variable), this removes the need of declaration above and assingment done on line next to this
+     //private renderer: Renderer2;
 
+    //constructor(element: ElementRef) {
+        constructor(private element: ElementRef, private renderer: Renderer2) {  //this creates a private variable name element of type ElementRef and assigns whatever value gets injected(template reference variable), this removes the need of declaration above and assingment done on line next to this
+        //this.renderer = renderer // A quick way of assigning if as above
         //this.element = element; // A quick way of assigning if as above
         // this.element.nativeElement.style.backgroundColor = 'Red';
         // this.element.nativeElement.style.color = 'Black';
@@ -17,8 +19,13 @@ export class SetBackground implements OnInit {
     }
 
     ngOnInit() {
-        this.element.nativeElement.style.backgroundColor = '#36454F';
-        this.element.nativeElement.style.color = 'White';
+        // this.element.nativeElement.style.backgroundColor = '#36454F';
+        // this.element.nativeElement.style.color = 'White';
+        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', '#36454F');
+        this.renderer.setStyle(this.element.nativeElement, 'color', 'White');
+        this.renderer.setAttribute(this.element.nativeElement, 'title', 'Product Attributes');//This assigns title attribute to each span element, Product Attributes acts as a tooltip 
+        // this.renderer.addClass();   //adds css class
+        // this.renderer.removeClass();   //adds css class
     }
 
 }
