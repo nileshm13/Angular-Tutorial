e901f832eca69d6c1ffb382668e444d571e9311b
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-04 02:11:15 +0530

    Observables: Error Handling and Complete

--- angular-services-and-more/src/app/observables/observables.component.ts
+++ angular-services-and-more/src/app/observables/observables.component.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./observables.component.css']
 })
 export class ObservablesComponent {
-  data: any[]=[];
+  data: any[] = [];
 
   //Observable
   sampleOservable = new Observable((observer) => {
@@ -21,7 +21,24 @@ export class ObservablesComponent {
     setTimeout(() => { observer.next(3) }, 3000);
     setTimeout(() => { observer.next(4) }, 4000);
     setTimeout(() => { observer.next(5) }, 5000);
-  })
+  });
+
+  observerWithError = new Observable((observer) => { //This will help to notice data getting sent as stream rather than all at once (due to timeout)    
+    setTimeout(() => { observer.next(1) }, 1000);
+    setTimeout(() => { observer.next(2) }, 2000);
+    setTimeout(() => { observer.error(new Error('Something went wong, plese try again later !!')) }, 3000);
+    setTimeout(() => { observer.next(4) }, 4000);
+    setTimeout(() => { observer.next(5) }, 5000);
+  });
+
+  observerWithComplete = new Observable((observer) => { //This will help to notice data getting sent as stream rather than all at once (due to timeout)    
+    setTimeout(() => { observer.next(1) }, 1000);
+    setTimeout(() => { observer.next(2) }, 2000);
+    setTimeout(() => { observer.next(3) }, 3000);
+    setTimeout(() => { observer.next(4) }, 4000);
+    setTimeout(() => { observer.next(5) }, 5000);
+    setTimeout(() => { observer.complete() }, 6000);
+  });
 
 
   getData() {
@@ -37,4 +54,33 @@ export class ObservablesComponent {
       this.data.push(value);
     });
   }
+
+  getErrorData() {
+    this.observerWithError.subscribe({
+      next: (value: any) => { //If FAT Arrow symbol is not used, it looks for data property within this subscribe object and as not found, it sends undefined to template
+        this.data.push(value);
+      },
+      error(err) {
+        alert(err)
+      },
+      complete() {
+        alert('All data has been fetched');
+      }
+    });
+  }
+
+  getCompleteData() {
+    this.observerWithComplete.subscribe({
+      next: (value: any) => {//If FAT Arrow symbol is not used, it looks for data property within this subscribe object and as not found, it sends undefined to template
+        this.data.push(value);
+      },
+      error(err) {
+        alert(err)
+      },
+      complete() {
+        alert('All data has been fetched');
+      }
+    });
+  }
+
 }
