67845439d7953246999de21ef4b33cb90b230eec
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-03 20:52:58 +0530

    Services - Subscribe Implementation DI implementation to subscribe to services in a class.

--- angular-services-and-more/src/app/header/home/hero/hero.component.ts
+++ angular-services-and-more/src/app/header/home/hero/hero.component.ts
@@ -1,9 +1,21 @@
 import { Component, inject } from '@angular/core';
+import { SubscriptionService } from 'src/app/Services/Subsciption.service';
 
 @Component({
   selector: 'app-hero',
-  templateUrl: './hero.component.html'
+  templateUrl: './hero.component.html',
+  providers: [SubscriptionService] //What to provide, like list of services that need to be injected in this class
 })
+
 export class HeroComponent {
+  type: string = 'My Site premium content'
+  //How to Provide Dependency Injection
+  constructor(private subscriptionService: SubscriptionService) {
+
+  }
+
+  PremiumSubscription() {
+    this.subscriptionService.OnSubscription(this.type);
+  }
 
 }
--- angular-services-and-more/src/app/header/home/sidebar/sidebar.component.ts
+++ angular-services-and-more/src/app/header/home/sidebar/sidebar.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { SubscriptionService } from 'src/app/Services/Subsciption.service';
 
 @Component({
   selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html'
+  templateUrl: './sidebar.component.html',
+  providers: [SubscriptionService] //What to provide, like list of services that need to be injected in this class
 })
 export class SidebarComponent {
+  type: string = 'My Site for a month';
+  //How to Provide Dependency Injection
+  constructor(private subscriptionService: SubscriptionService) {
+  }
 
+  MonthlySubscription() {
+    this.subscriptionService.OnSubscription(this.type);
+  }
 }
