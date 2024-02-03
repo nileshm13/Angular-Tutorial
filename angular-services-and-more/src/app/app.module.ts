2b1b09adb1b896c31c0095460c40fd2a13a6d2fb
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-04 00:44:20 +0530

    Rollbacking previous change

--- angular-services-and-more/src/app/app.module.ts
+++ angular-services-and-more/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, InjectionToken } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -13,10 +13,6 @@ import { UserDetailComponent } from './header/admin/user-detail/user-detail.comp
 import { SubscriptionService } from './Services/Subsciption.service';
 import { UserService } from './Services/user.service';
 import { LoggerService } from './Services/logger.service';
-
-export const USER_TOKEN = new InjectionToken<UserService>('USER_SERVICE_TOKEN');
-// USER_TOKEN is unique identifier which will be assigned to provide in providers Array
-// USER_SERVICE_TOKEN is just a string to identify what this token is created for, can be any string
 
 @NgModule({
   declarations: [
@@ -33,11 +29,8 @@ export const USER_TOKEN = new InjectionToken<UserService>('USER_SERVICE_TOKEN');
     BrowserModule,
     FormsModule
   ],
-  // Each element in providers array is actually treated by angular as {provide: UserService, useClass: UserService}
-  // provide is used to uniquely identify a provider in providers array, it accepts values such as Type,string,instance of injection token. Its also called as token.
-  // useClass is to identify which class needs to be instantiated.
-  providers: [SubscriptionService, {provide: USER_TOKEN, useClass: UserService}, LoggerService],
-  // providers: [SubscriptionService, UserService, LoggerService],
+  
+  providers: [SubscriptionService, UserService, LoggerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-services-and-more/src/app/header/admin/user-list/user-list.component.ts
+++ angular-services-and-more/src/app/header/admin/user-list/user-list.component.ts
@@ -1,14 +1,13 @@
-import { Component, Inject, inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { User } from 'src/app/Models/User';
 import { UserService } from 'src/app/Services/user.service';
-import { USER_TOKEN } from 'src/app/app.module';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
 })
 export class UserListComponent {
-  constructor(@Inject(USER_TOKEN) private userService: UserService) {
+  constructor(private userService: UserService) {
 
   } 
 
