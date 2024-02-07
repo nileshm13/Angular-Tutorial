f55f266fa6fa6953744a705955e9d5b2832e5335
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-08 01:33:25 +0530

    RouteParams Passing data from Courses page to Course details page based on id route param

--- angular-routing/src/app/courses/course-detail/course-detail.component.ts
+++ angular-routing/src/app/courses/course-detail/course-detail.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from 'src/app/Models/course';
+import { CourseService } from 'src/app/Services/course.service';
 
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
   styleUrls: ['./course-detail.component.css']
 })
-export class CourseDetailComponent{
+export class CourseDetailComponent implements OnInit {
+  courseId: number
+  selectedCourse: Course;
+  currentRoute: ActivatedRoute = inject(ActivatedRoute);
+  courseService: CourseService = inject(CourseService);
 
+  ngOnInit() {
+    //this.courseId = this.currentRoute.snapshot.params['id'];  //old way of retreiving route params
+    // console.log(this.courseId);
+    //this.courseId = Number(this.currentRoute.snapshot.paramMap.get('id')); //newer and preferred way, returns string
+    // console.log(this.courseId);
+
+    //this.selectedCourse = this.courseService.courses.find(crs => crs.id === this.courseId);
+    //console.log(this.selectedCourse);
+
+    /*Above methods will not work in real time as ngOnInit is called only once,so courseId will be set only once, after this, if user navigates to next/prev page using buttons,
+    courseId value will be incorrect and so page rendering will not change, just the url will change as its handled in html, to handle this, we use observables as below, this will emit 
+    a new value on every page change and the subscription method will be called where logic on page change can be taken care of */
+    this.currentRoute.paramMap.subscribe((data)=>
+    {
+      this.courseId = Number(data.get('id'));
+      this.selectedCourse = this.courseService.courses.find(crs => crs.id === this.courseId);
+    });
+
+  }  
 }
--- angular-routing/src/app/home/popular/popular.component.ts
+++ angular-routing/src/app/home/popular/popular.component.ts
@@ -20,7 +20,7 @@ export class PopularComponent {
 
   openCourses() {
     //Its absolute. It accepts an array where we specify each route to be navigated so for localhost:4200/courses/python/3.5 , we specify this.route.navigate(['courses','python','3.5']); 
-    // this.route.navigate(['courses']);
+     this.route.navigate(['courses']);
     // this.route.navigate(['courses','python','3.5']);
 
      //Its absolute. It accepts a string where we specify the link to be directed to, so for localhost:4200/courses/python/3.5 , we specify this.route.navigateByUrl('courses/python/3.5');
@@ -30,6 +30,6 @@ export class PopularComponent {
     //Both of above are absolute in nature by default but we can use navigateByUrl and provide relativeTo to provide redirection relative to a current url
     //relativeTo accepts type activatedRoute which specifies crurent url. 
     // To get current url, use ActivatedRoute --> currentURL: ActivatedRoute = inject(ActivatedRoute);
-    this.route.navigate(['courses'], {relativeTo: this.currentURL }); //This will redirect to http://localhost:4200/home/courses
+    //this.route.navigate(['courses'], {relativeTo: this.currentURL }); //This will redirect to http://localhost:4200/home/courses
   }
 }
