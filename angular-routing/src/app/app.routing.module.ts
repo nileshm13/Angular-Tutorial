ca1c2cb4d01ab7134eb994fa0488aaf06e7cd675
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-08 18:11:10 +0530

    Angular Routing: Query params, Fragments and Routing module creation

---
+++ angular-routing/src/app/app.routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { CourseDetailComponent } from "./courses/course-detail/course-detail.component";
+import { CoursesComponent } from "./courses/courses.component";
+import { HomeComponent } from "./home/home.component";
+import { PopularComponent } from "./home/popular/popular.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    // { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'courses', component: CoursesComponent },
+    {
+        path: 'courses', children: [
+            { path: 'course/:id', component: CourseDetailComponent },
+            { path: 'popular', component: PopularComponent }
+        ]
+    },
+    // { path: 'courses/course/:id', component: CourseDetailComponent },
+    { path: '**', component: NotFoundComponent }
+]
+
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class RoutingModule {
+
+}
--- angular-routing/src/app/header/header.component.ts
+++ angular-routing/src/app/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  
+export class HeaderComponent implements OnInit {
+  currentRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  ngOnInit() {
+    this.currentRoute.fragment.subscribe((data) => {
+      if (data) {
+        document.getElementById(data).scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  }
 }
