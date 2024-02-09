a1a14319acc9b39ee4b7e7d5e64d3684d4223ded
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-10 00:46:56 +0530

    Passing dynamic data to component from routerLink using [state]

--- angular-routing/src/app/checkout/checkout.component.ts
+++ angular-routing/src/app/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Course } from '../Models/course';
 
 @Component({
   selector: 'app-checkout',
@@ -8,12 +9,18 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class CheckoutComponent implements OnInit {
   activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  router: Router = inject(Router);
+  course: Course;
 
   ngOnInit(): void {
     //Static data passed from router module using data property
-    this.activatedRoute.data.subscribe((res) => {
-      console.log(res);
-    })
+    // this.activatedRoute.data.subscribe((res) => {
+    //   console.log(res);
+    // })
+
+    //Dynamic data passed from routerLink of a component using [state]
+    this.course = history.state;
+    //console.log(this.router.getCurrentNavigation().extras.state); //This can be assigned in a constructor only 
   }
 
 }
