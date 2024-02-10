c0963ee31a79c7a79d2dd7fe3c0e6fe628d0fb59
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-10 23:55:39 +0530

    Template Driven Forms: Basics, creating ngSubmit and printing form controls

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven-form';
+  @ViewChild('registrationForm') regForm: NgForm;
+
+  submitForm() {
+    console.log(this.regForm);
+    //Printing from controls section of Form Controls 
+    console.log(this.regForm.controls["firstname"].value);
+    console.log(this.regForm.controls["lastname"].value);
+    console.log(this.regForm.controls["emailAdd"].value);
+    //Printing from values section of Form Contol
+    console.log(this.regForm.value.phoneNo);
+    console.log(this.regForm.value.dob);
+    console.log(this.regForm.value.usrname);
+    console.log(this.regForm.controls["firstname"]);
+    console.log(this.regForm.controls["lastname"]);
+    console.log(this.regForm.controls["emailAdd"]);
+  }
 }
