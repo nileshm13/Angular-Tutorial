2f918c4162e2a2aaee643a6149e0fa4fa9ea23ad
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-11 01:01:14 +0530

    Template Driven Form : Validations

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -9,8 +9,16 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'template-driven-form';
   @ViewChild('registrationForm') regForm: NgForm;
+  genders =  [
+    { id: 'check-male', value: 'male', display: 'Male' },
+    { id: 'check-female', value: 'female', display: 'Female' },
+    { id: 'check-other', value: 'prefer not to say', display: 'Prefer not to say' }
+  ]
+  @ViewChild('fname') firstname: HTMLInputElement;
+ 
 
   submitForm() {
+    console.log(this.firstname);
     console.log(this.regForm);
     //Printing from controls section of Form Controls 
     console.log(this.regForm.controls["firstname"].value);
