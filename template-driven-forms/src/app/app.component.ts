d78aaeb1d0a9b6c8aa31fc64c95418c62c39e29e
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-11 18:09:00 +0530

    Template Driven Form: ngGroupModel, setValue, patchValue, form reset

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -18,12 +18,38 @@ export class AppComponent {
   lastName: string;
   emailAddress: string;
   usernameBtnClicked: boolean = false;
-  username: string;
   dateOfBirth: string;
+  phoneNo: string;
+  userName: string;
+  userGender: string;
+  line1Address: string;
+  line2Address: string
+  userCountry: string
+  userCity: string
+  userRegion: string
+  postCode: string
+  agreementSelected: boolean
+  formSubmitted: boolean = false;
 
   submitForm() {
-
+    this.formSubmitted = true;
+    this.usernameBtnClicked = false;
+    console.log("BEFORE RESET");
     console.log(this.regForm);
+    console.log(this.firstName);
+    console.log(this.lastName);
+    console.log(this.emailAddress);
+    console.log(this.phoneNo);
+    console.log(this.dateOfBirth);
+    console.log(this.userName);
+    console.log(this.userGender);
+    console.log(this.line1Address);
+    console.log(this.line2Address);
+    console.log(this.userCity);
+    console.log(this.userCountry);
+    console.log(this.userRegion);
+    console.log(this.postCode);
+    console.log(this.agreementSelected);
     // //Printing from controls section of Form Controls 
     // console.log(this.regForm.controls["firstname"].value);
     // console.log(this.regForm.controls["lastname"].value);
@@ -35,28 +61,76 @@ export class AppComponent {
     // console.log(this.regForm.controls["firstname"]);
     // console.log(this.regForm.controls["lastname"]);
     // console.log(this.regForm.controls["emailAdd"]);
-    
+
+    this.regForm.reset();
+    console.log("AFTER RESET");
+    console.log(this.regForm);
+    console.log(this.firstName);
+    console.log(this.lastName);
+    console.log(this.emailAddress);
+    console.log(this.phoneNo);
+    console.log(this.dateOfBirth);
+    console.log(this.userName);
+    console.log(this.userGender);
+    console.log(this.line1Address);
+    console.log(this.line2Address);
+    console.log(this.userCity);
+    console.log(this.userCountry);
+    console.log(this.userRegion);
+    console.log(this.postCode);
+    console.log(this.agreementSelected);
   }
 
   createUserName() {
     this.usernameBtnClicked = true;
     if (this.firstName && this.lastName && this.dateOfBirth) {
+      let username = "";
       if (this.firstName.length > 3) {
-        this.username = this.firstName.slice(0, 3);
+        username = this.firstName.slice(0, 3);
       }
       else {
-        this.username = this.firstName;
+        username = this.firstName;
       }
 
       if (this.lastName.length > 3) {
-        this.username += this.lastName.slice(0, 3);
+        username += this.lastName.slice(0, 3);
       }
       else {
-        this.username += this.lastName;
+        username += this.lastName;
       }
 
       let tmpDate = new Date(this.dateOfBirth);
-      this.username += tmpDate.getFullYear();
+      username += tmpDate.getFullYear();
+      //this.regForm.value.usrname = username;
+
+      //Assigning Value using SetValue (If we use ngModel, this might not be required, for knowledge purpose we are using it)
+      // this.regForm.setValue({
+      //   firstname: this.regForm.value.firstname,
+      //   lastname: this.regForm.value.lastname,
+      //   phoneNo: this.regForm.value.phoneNo,
+      //   dob: this.regForm.value.dob,
+      //   usrname: username,
+      //   emailAdd: this.regForm.value.emailAdd,
+      //   adressSection: {
+      //     addressLine1: this.regForm.value.adressSection.addressLine1,
+      //     addressLine2: this.regForm.value.adressSection.addressLine2,
+      //     country: this.regForm.value.adressSection.country,
+      //     city: this.regForm.value.adressSection.city,
+      //     region: this.regForm.value.adressSection.region,
+      //     postalCode: this.regForm.value.adressSection.postalCode,
+      //   },
+      //   agreement: this.regForm.value.agreement,
+      //   gender: this.regForm.value.gender
+      // });
+
+      //Assigning Value using PatchValue
+      this.regForm.form.patchValue({
+        usrname: username,
+        adressSection: {
+          region: "Indian Subcontinent"
+        }
+      })
+
     }
   }
 }
