9c8e79772879e133709825f6350f11feb6f98eaa
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-23 01:19:39 +0530

    Reactive Forms: valueChanges, stateChanges, setValue, patchValue, form reset and form data assignment

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -11,14 +11,15 @@ export class AppComponent implements OnInit {
 
   title = 'template-driven-form';
   employeeForm: FormGroup;
+  employeeDetails: any = {};
 
   ngOnInit() {
     this.employeeForm = new FormGroup({
       fname: new FormControl(null, [Validators.required, CustomValidator.noSpace]),
       lname: new FormControl(null, [Validators.required, CustomValidator.noSpace]),
       email: new FormControl(null, [Validators.required, Validators.email]),
-      usrname: new FormControl(null,[], CustomValidator.usrNameAvailable),
-      dob: new FormControl(null),
+      usrname: new FormControl(null, Validators.required, CustomValidator.usrNameAvailable),
+      dob: new FormControl(null,Validators.required),
       gender: new FormControl('male'),
       address: new FormGroup({
         streetAddress: new FormControl(null, Validators.required),
@@ -31,14 +32,45 @@ export class AppComponent implements OnInit {
         new FormControl(null, Validators.required)
       ])
     });
+
+    //#region valueChanges
+    // this.employeeForm.valueChanges.subscribe((data) => {
+    //   console.log(data);
+    // });
+
+    // this.employeeForm.get('fname').valueChanges.subscribe((data) => {
+    //   console.log(data);
+    // });
+    //#endregion
+
+    //#region statusChanges
+    // this.employeeForm.statusChanges.subscribe((data)=>{
+    //   console.log(data);
+    // });
+
+    // this.employeeForm.get('fname').statusChanges.subscribe((data)=>{
+    //   console.log(data);
+    // });
+    //#endregion
   }
 
   submitDetails() {
     console.log(this.employeeForm);
+    //Assigning Form Value to an object
+    this.employeeDetails = this.employeeForm.value;
+    // console.log(this.employeeDetails);
+
+    //Resetting a form
+    this.employeeForm.reset({
+      gender: 'male',
+      address: {
+        country: 'India'
+      }
+    });
   }
 
   addSkill() {
-    //This returns an abstract control, but  it does ont have push method which we need to push FC into employeeForm group so we typecast. We know return type of skills is FormArray.
+    //This returns an abstract control, but it does ont have push method which we need to push FC into employeeForm group so we typecast. We know return type of skills is FormArray.
     //this.employeeForm.get('skills');
     (<FormArray>this.employeeForm.get('skills')).push(new FormControl(null));
     console.log(this.employeeForm.get('skills'));
@@ -48,4 +80,44 @@ export class AppComponent implements OnInit {
     (<FormArray>this.employeeForm.get('skills')).removeAt(index);
   }
 
+  createUserName() {
+    var date = new Date();
+    // //#region setValue for entire FormGroup
+    // this.employeeForm.setValue({
+    //   fname: this.employeeForm.get('fname').value,
+    //   lname: this.employeeForm.get('lname').value,
+    //   email: this.employeeForm.get('email').value,
+    //   usrname: this.employeeForm.get('fname').value + this.employeeForm.get('lname').value + date.getMilliseconds().toString(),
+    //   dob: this.employeeForm.get('dob').value,
+    //   gender: this.employeeForm.get('gender').value,
+    //   address: {
+    //     streetAddress: this.employeeForm.get('address.streetAddress').value,
+    //     country: this.employeeForm.get('address.country').value,
+    //     city: this.employeeForm.get('address.city').value,
+    //     region: this.employeeForm.get('address.region').value,
+    //     postalCode: this.employeeForm.get('address.postalCode').value
+    //   },
+    //   skills: this.employeeForm.get('skills').value
+    // });
+    // //#endregion
+
+    // //#region setValue for single FormControl
+    // this.employeeForm.get('usrname').setValue(
+    //   this.employeeForm.get('fname').value + this.employeeForm.get('lname').value + date.getMilliseconds().toString()
+    // );
+    // //#endRegion setValue for single FormControl
+
+    //#region patchValue for FormGroup
+    // this.employeeForm.patchValue({
+    //   usrname: this.employeeForm.get('fname').value + this.employeeForm.get('lname').value + date.getMilliseconds().toString(),
+    //   address: {
+    //     region: 'Asia'
+    //   }
+    // });
+    //#endregion
+
+    //#region patchValue for FormControl
+    this.employeeForm.get('usrname').patchValue(this.employeeForm.get('fname').value + this.employeeForm.get('lname').value + date.getMilliseconds().toString());
+    //#endregion
+  }
 } 
