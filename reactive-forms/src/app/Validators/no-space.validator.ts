7048a3d698ffe5fa1a0283ca9f3031881d783499
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-22 16:52:49 +0530

    Reactive Form: Custom sync validator using function without class

---
+++ reactive-forms/src/app/Validators/no-space.validator.ts
@@ -0,0 +1,12 @@
+import { FormControl } from "@angular/forms";
+
+//Create a const variable noSpaceValidator
+//Assign a function to it
+//Add param, in this case, it accepts form control
+//This is one way, another is to use Class and then static method inside it, see below 
+export const noSpaceValidator = (control: FormControl) => {
+    if (control.value != null && control.value.indexOf(' ') !== -1) {
+        return { noSpace: true }
+    }
+    return null;
+}
