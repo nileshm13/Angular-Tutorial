d0805cb74c6f7b6a739f9df84efe8af12d9e2cd3
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-24 00:32:44 +0530

    Commiting missed changes

--- angular-http/src/app/dashboard/dashboard.component.ts
+++ angular-http/src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { TaskModel } from '../Models/Task';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
   showCreateTaskForm: boolean = false;
   task: TaskModel;
   http: HttpClient = inject(HttpClient);
+
+  ngOnInit() {
+    
+  }
 
   OpenCreateTaskForm() {
     this.showCreateTaskForm = true;
@@ -21,12 +25,14 @@ export class DashboardComponent {
   }
 
   submitTaskForm(value: TaskModel) {
+    //const taskHeaders = new HttpHeaders({ 'testheader1': 'value1' });    
     this.task = value;
-    //this.CloseCreateTaskForm();
+    this.CloseCreateTaskForm();
     //HTTP call will be made only if there is a subsciber to it.
-    this.http.post('https://angularhttp-c69fb-default-rtdb.firebaseio.com/tasks.json', this.task).subscribe((res) => {
-      console.log(res);
-      //generates a key value pair, key is randomly generated whereas value is entire task object
-    });
+    this.http.post<string>('https://angularhttp-c69fb-default-rtdb.firebaseio.com/tasks.json',
+      this.task, { headers: { 'testheader1': 'value1', 'testheader2': 'value2' } }).subscribe((res) => {
+        console.log(res);
+        //generates a key value pair, key is randomly generated whereas value is entire task object
+      });
   }
 }
