d4234b823d2f9b9de1a5f669d5991ec83f30bc1f
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-02-24 22:57:39 +0530

    Angular HTTP: GetById, QueryParams and Headers

--- angular-http/src/app/Services/task.service.ts
+++ angular-http/src/app/Services/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from "@angular/core";
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { TaskModel } from "../Models/Task";
 import { Observable, map } from "rxjs";
 
@@ -17,9 +17,20 @@ export class TaskService {
     }
 
     getAllTasks() {
-        let getURL = this.firebaseURL + '/tasks.json'
+        let getURL = this.firebaseURL + '/tasks.json';
+        let reqHeaders = new HttpHeaders(); //This is immutable is nature, hence we have to reassign in below line
+        reqHeaders = reqHeaders.set('test-header1', 'header-value1');
+        reqHeaders = reqHeaders.set('test-header2', 'header-value2');
+        // reqHeaders = reqHeaders.append('test-header1','header-value3');
+        // reqHeaders = reqHeaders.append('test-header2','header-value4');
+
+        let queryParams = new HttpParams();
+        queryParams = queryParams.set('page', '2');
+        queryParams = queryParams.set('items', '10');
+        queryParams = queryParams.append('page', '3');
+        queryParams = queryParams.append('items', '15');
         let tasks = [];
-        return this.http.get(getURL).pipe(map((response) => {
+        return this.http.get(getURL, { headers: reqHeaders, params: queryParams }).pipe(map((response) => {
             for (let key in response) {
                 if (response.hasOwnProperty(key)) {
                     tasks.push({ ...response[key], 'id': key });
@@ -43,5 +54,9 @@ export class TaskService {
     updateTask(id: string, task: TaskModel) {
         let updateURL = this.firebaseURL + '/tasks/' + id + '.json'
         return this.http.put(updateURL, task);
+    }
+
+    getTaskDetailsById(id: string) {
+        return this.http.get<TaskModel>(this.firebaseURL + '/tasks/' + id + '.json');
     }
 }
--- angular-http/src/app/dashboard/task-details/task-details.component.ts
+++ angular-http/src/app/dashboard/task-details/task-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TaskModel } from 'src/app/Models/Task';
 
 @Component({
   selector: 'app-task-details',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-details.component.css']
 })
 export class TaskDetailsComponent {
+  @Output() showTaskDetailsEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input() selectedTask: TaskModel;
 
+  closePopup() {
+    this.showTaskDetailsEvent.emit(false);
+  }
 }
