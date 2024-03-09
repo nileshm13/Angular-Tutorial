75ef0d49cf31d5bd3e6f4e07bca6555f0d0f3774
Nilesh Mehta <nileshmehta1996@outlook.com>
2024-03-09 20:34:45 +0530

    Auth : AutoLogin implementation

--- angular-http/src/app/Models/UserModel.ts
+++ angular-http/src/app/Models/UserModel.ts
@@ -1,13 +1,16 @@
 export class UserModel {
     constructor(
         public userId: string,
-        public email: string,        
+        public email: string,
         private _expiresIn: Date,
-        private _tokenId: string        
+        private _tokenId: string
     ) { }
 
     //This acts as a property to get token for logged in user as field is private
-    get token() {
+    get token() {        
+        if (this._expiresIn && this._expiresIn < new Date()) {
+            return null;
+        }
         return this._tokenId;
     }
 }
--- angular-http/src/app/app.component.ts
+++ angular-http/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { LoginService } from './Services/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-http-client';
+  loginService: LoginService = inject(LoginService);
+
+  ngOnInit(): void {
+    this.loginService.isLoggedIn();
+  }
 }
